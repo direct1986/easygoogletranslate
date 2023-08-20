b43beea9d42a9552197d20e8d6c7b4971706400c
direct1986 <07ky@sina.com>
2023-08-20 11:42:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from easygoogletranslate import EasyGoogleTranslate
 
 translator = EasyGoogleTranslate(
-    source_language='en',
-    target_language='de',
+    source_language='ko',
+    target_language='zh-CN',
     timeout=10
 )
-result = translator.translate('This is an example.')
+result = translator.translate('소브산 부적합[결과 : 0.0148g/kg 검출/ 규격: 불검출]')
 
 print(result) 
 # Output: Dies ist ein Beispiel.
